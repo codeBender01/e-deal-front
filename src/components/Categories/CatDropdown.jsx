9e98cb7b774306987f7b39f709314f8c54d5a255
codeBender01 <ahmetsaparow.mr@gmail.com>
2022-06-24 02:04:22 +0300

    adding categories component that displays on hover

--- src/components/Categories/CatDropdown.jsx
+++ src/components/Categories/CatDropdown.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import comics from "../../images/comics.webp";
 
-function CatDropdown() {
-  return <div></div>;
+function CatDropdown(props) {
+  // drop.current.style.left = xPos;
+
+  return props.categories.map((cat) => {
+    return (
+      <li key={cat.id}>
+        <h3>{cat.name}</h3>
+        <div className="dr-hover">
+          <div className="cat-list">
+            <h4>Category Name</h4>
+            <div className="line"></div>
+            <ul>
+              {cat.subCats.map((subCat) => {
+                return <li key={subCat}>{subCat}</li>;
+              })}
+            </ul>
+          </div>
+          <div className="cat-img">
+            <img src={comics} alt="comics" />
+          </div>
+        </div>
+      </li>
+    );
+  });
 }
 
 export default CatDropdown;
--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,63 +1,160 @@
 import React from "react";
+import CatDropdown from "./CatDropdown";
 import "./categories.scss";
 
 const categories = [
   {
     id: 1,
     name: "Home",
+    subCats: [
+      "Art",
+      "Coins & paper money",
+      "Comics",
+      "Collectible card games",
+      "Bullion",
+      "Sport trading cards",
+      "Antiques",
+    ],
   },
   {
     id: 2,
     name: "Saved",
+    subCats: [
+      "Art",
+      "Coins & paper money",
+      "Comics",
+      "Collectible card games",
+      "Bullion",
+      "Sport trading cards",
+      "Antiques",
+    ],
   },
   {
     id: 3,
     name: "Motors",
+    subCats: [
+      "Art",
+      "Coins & paper money",
+      "Comics",
+      "Collectible card games",
+      "Bullion",
+      "Sport trading cards",
+      "Antiques",
+    ],
   },
   {
     id: 4,
     name: "Electronics",
+    subCats: [
+      "Art",
+      "Coins & paper money",
+      "Comics",
+      "Collectible card games",
+      "Bullion",
+      "Sport trading cards",
+      "Antiques",
+    ],
   },
   {
     id: 5,
     name: "Collectibles & Art",
+    subCats: [
+      "Art",
+      "Coins & paper money",
+      "Comics",
+      "Collectible card games",
+      "Bullion",
+      "Sport trading cards",
+      "Antiques",
+    ],
   },
   {
     id: 6,
     name: "Home & Garden",
+    subCats: [
+      "Art",
+      "Coins & paper money",
+      "Comics",
+      "Collectible card games",
+      "Bullion",
+      "Sport trading cards",
+      "Antiques",
+    ],
   },
   {
     id: 7,
     name: "Clothing & Accesories",
+    subCats: [
+      "Art",
+      "Coins & paper money",
+      "Comics",
+      "Collectible card games",
+      "Bullion",
+      "Sport trading cards",
+      "Antiques",
+    ],
   },
   {
     id: 8,
     name: "Toys",
+    subCats: [
+      "Art",
+      "Coins & paper money",
+      "Comics",
+      "Collectible card games",
+      "Bullion",
+      "Sport trading cards",
+      "Antiques",
+    ],
   },
   {
     id: 9,
     name: "Sport",
+    subCats: [
+      "Art",
+      "Coins & paper money",
+      "Comics",
+      "Collectible card games",
+      "Bullion",
+      "Sport trading cards",
+      "Antiques",
+    ],
   },
   {
     id: 10,
     name: "Business & Industrial",
+    subCats: [
+      "Art",
+      "Coins & paper money",
+      "Comics",
+      "Collectible card games",
+      "Bullion",
+      "Sport trading cards",
+      "Antiques",
+    ],
   },
-  {
-    id: 11,
-    name: "Jewelry & Watches",
-  },
+  // {
+  //   id: 11,
+  //   name: "Jewelry & Watches",
+  //   subCats: [
+  //     "Art",
+  //     "Coins & paper money",
+  //     "Comics",
+  //     "Collectible card games",
+  //     "Bullion",
+  //     "Sport trading cards",
+  //     "Antiques",
+  //   ],
+  // },
 ];
 
 function Categories() {
-  const renderedCategories = () => {
-    return categories.map((cat) => {
-      return <li key={cat.id}>{cat.name}</li>;
-    });
-  };
   return (
     <div className="categories">
       <div className="container">
-        <ul>{renderedCategories()}</ul>
+        <ul className="main-cats">
+          <CatDropdown categories={categories} />
+        </ul>
       </div>
     </div>
   );
